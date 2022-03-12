00601ec814dd27134d40b86adacf17b9cbf72d7e
ignasiperez <ignasiperez@gmail.com>
2022-03-12 13:21:17 +0100

    Step04 – Tests01-03 Refactoring3

---
+++ VCCompositionWithStoryboards_pt2-Step04/TestingVCCompositionWithStoryboards/TestingVCCompositionWithStoryboardsTests/PlayerScoreVCStoryboardTests.swift
@@ -0,0 +1,66 @@
+//
+//  PlayerScoreVCStoryboardTests.swift
+//  TestingVCCompositionWithStoryboardsTests
+//
+//  Created by Ignasi Perez-Valls on 2/3/22.
+//
+
+
+import XCTest
+@testable import TestingVCCompositionWithStoryboards
+
+
+class PlayerScoreVCStoryboardTests: XCTestCase {
+  
+  // Test 01
+  func test_PlayerScoreOneStoryboardInitialVC_isPlayerScoreVC() {
+    XCTAssertTrue(
+      makePlayerOneStoryboard().instantiateInitialViewController() is PlayerScoreVC
+    )
+  }
+  
+  
+  // Test 02
+  func test_PlayerScoreOneStoryboard_nameSetter_updatesNameLabel() {
+    // GIVEN
+    let vc = makePlayerScoreVC(storyboard: makePlayerOneStoryboard())
+    
+    
+    // WHEN
+    vc.name = "a name"
+
+    // THEN
+    XCTAssertEqual(vc.nameLabel?.text, "a name")
+  }
+  
+  
+  // Test 03
+  func test_PlayerScoreOneStoryboard_scoreSetter_updatesScoreLabel() {
+    // GIVEN
+    let vc = makePlayerScoreVC(storyboard: makePlayerOneStoryboard())
+    
+    // WHEN
+    vc.score = "a score"
+
+    // THEN
+    XCTAssertEqual(vc.scoreLabel?.text, "a score")
+  }
+  
+  
+  
+  // MARK: Helpers
+  
+  private func makePlayerOneStoryboard() -> UIStoryboard {
+    return UIStoryboard(name: "PlayerScoreOne",
+                        bundle: nil)
+  }
+  
+  
+  private func makePlayerScoreVC(storyboard: UIStoryboard) -> PlayerScoreVC {
+    let vc =  storyboard.instantiateInitialViewController() as! PlayerScoreVC
+    _ = vc.view
+    
+    return vc
+  }
+
+}
